a42b3c6d3007f7743bc02e01313bc8f84c2ea5d9
Sam Evans-Turner <sam.evans-turner@live.com.au>
2024-08-13 17:48:31 +1000

    Add config system, shared between server and webapp

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,7 @@
 module github.com/samevansturner/singstarswap
 
 go 1.22.6
+
+require (
+    github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
+)
--- server/main.go
+++ server/main.go
@@ -2,15 +2,115 @@ package main
 
 import (
 	"embed"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/kirsle/configdir"
 )
+
+type ServerConfig struct {
+	Port string `json:"port"`
+}
+
+type PS3Config struct {
+	Address string `json:"address"`
+	PS2Path string `json:"ps2path"`
+	PS3Path string `json:"ps3path"`
+}
+
+type Config struct {
+	ServerConfig `json:"server"`
+	PS3Config    `json:"PS3"`
+}
+
+// In essence this is a const setting for defaults, but Go doesn't work that way for structs afaict
+func GetDefaultSettings() *Config {
+	return &Config{ServerConfig{"4000"}, PS3Config{"192.168.1.2", "/dev_hdd0/SINGSTAR", "/net0/PS3ISO"}}
+}
 
 // Embed the static generated Angular from the previous build step.
 //
 //go:embed static
 var embedWebApp embed.FS
 
+// Set-up global config to be passed to web-app
+var settings *Config
+
+// Reads config from disk, and then saves it or re-saves it update defaults.
+func ReadConfig() (*Config, error) {
+
+	_settings := GetDefaultSettings()
+
+	configPath := configdir.LocalConfig("SingStarSwap")
+	err := configdir.MakePath(configPath)
+	if err != nil {
+		return nil, err
+	}
+	configFile := filepath.Join(configPath, "settings.json")
+
+	if _, err = os.Stat(configFile); !os.IsNotExist(err) {
+
+		file, err := os.Open(configFile)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		decoder := json.NewDecoder(file)
+		if err := decoder.Decode(&_settings); err != nil {
+			return nil, err
+		}
+		//If the file exists, we have read the data. OK to re-write the file now.
+	}
+
+	file, err := os.Create(configFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(&_settings); err != nil {
+		return nil, err
+	}
+
+	return _settings, nil
+}
+
+func returnConfig(w http.ResponseWriter, r *http.Request) {
+	// Convert your config to JSON
+	configJSON, err := json.Marshal(settings.PS3Config)
+	if err != nil {
+		http.Error(w, "Error encoding config", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(configJSON)
+}
+
 func main() {
+	var err error
+	settings, err = ReadConfig()
+	if err != nil {
+		panic(err)
+	}
 
-	// Display usage/help message
+	fmt.Print(settings)
 
+	fsys, err := fs.Sub(embedWebApp, "static")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fileserver := http.FileServer(http.FS(fsys))
+	http.Handle("/", fileserver)
+	http.Handle("/settings.json", http.HandlerFunc(returnConfig))
+
+	http.ListenAndServe(":4000", nil)
 }
